fba2e55cd47c75e05fc16c7392ce45b0c3163ed6
GhatnekarPreeti <preeti.ghatnekar@embold.io>
2021-03-05 10:43:02 +0530

    testscase

--- ResourceLeak.java
+++ ResourceLeak.java
@@ -18,7 +18,7 @@ public class ResourceLeak {
         try {
             PrintWriter out = new PrintWriter("");
             out.println("the text");
-            out.close();
+        
             Cipher c1 = Cipher.getInstance("DES");
         } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException e) {
            
