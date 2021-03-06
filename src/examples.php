eface278f651fe5802cd6b50dd2e0872ad9dafc5
GhatnekarPreeti <preeti.ghatnekar@embold.io>
2021-03-06 17:23:32 +0530

    added files

---
+++ src/examples.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Developer News</h1>
+
+<?php echo "The Best PHP Examples";
+class Foo {
+	    public function bar($flag = true) {
+	    }
+	}
+
+ ?>
+
+</body>
+</html> 
