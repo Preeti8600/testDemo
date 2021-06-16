acafd4666419de1b66d051e0fdfb49fd9c25cbab
Preeti8600 <33160356+Preeti8600@users.noreply.github.com>
2021-06-16 19:32:43 +0530

    Update ResourceLeak1.java

--- src/ResourceLeak1.java
+++ src/ResourceLeak1.java
@@ -12,7 +12,32 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class ResourceLeak1 {
-	
+	  public void process() {
+        SecureRandom secureRandom = new SecureRandom();
+	     System.runFinalizersOnExit(true);
+        try {    
+            PrintWriter out = new PrintWriter("");
+            out.println("the text");
+
+System.out.println("");
+System.out.println("");   
+            out.close();
+            Cipher c1 = Cipher.getInstance("DES");
+        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException e) {
+            System.err.println();
+System.err.println();
+
+
+        
+            Cipher c1 = Cipher.getInstance("DES");
+        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException e) {
+           
+
+
+        }
+
+    }
+   
 	
 	
 	
